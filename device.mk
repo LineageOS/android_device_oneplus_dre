1bb9af1c1fc0fd049cd9a0b853985e7c94fa5f59
Bowgo Tsai <bowgotsai@google.com>
2020-03-03 12:15:50 +0800

    dre: Removing GSI keys
    Those keys will be embedded into VtsSecurityAvb on host side
    instead, to verify the GSI image used on the device.
    
    Bug: 149806769
    Test: build and checks those keys are removed from
          $OUT/recovery/root/first_stage_ramdisk/avb/
    Change-Id: I8a002ba6f1421fb460056ccae6572050bdb0ce3c

--- device.mk
+++ device.mk
@@ -23,9 +23,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# Include GSI keys
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
