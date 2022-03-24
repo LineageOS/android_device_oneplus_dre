9a431a232212df5fcc4e8750db884b037449893d
althafvly <althafvly@gmail.com>
2022-03-24 17:18:43 +0530

    dre: Inherit launch_with_vendor_ramdisk for vAB
    - sm4350 devices have a vendor_boot partition
    
    Change-Id: I5c344890bc12ee769e2f27369a4151b469271a51

--- device.mk
+++ device.mk
@@ -10,8 +10,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Enable virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+# Enable virtual A/B OTA with virtual ramdisk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
