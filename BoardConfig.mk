537445c308e337231f82303470e05979e976b249
LuK1337 <priv.luk@gmail.com>
2022-03-30 00:30:07 +0200

    dre: Include vendor/debugfs.config
    This disables the following kernel configs:
    * CONFIG_DEBUG_FS
    * CONFIG_PAGE_OWNER
    
    Change-Id: Idbf74e3dee4dccff4708462271e79374e28449ae

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,7 +111,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm4350
-TARGET_KERNEL_CONFIG := vendor/holi-qgki_defconfig
+TARGET_KERNEL_CONFIG := vendor/holi-qgki_defconfig vendor/debugfs.config
 TARGET_KERNEL_NO_GCC := true
 
 # Kernel modules
