22fa99ecf367e8df347691263f3f9f535a383a10
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 21:39:56 -0700

    dre: overlay: Import relevant custom changes made prior to RROs
    Change-Id: Id3e5c5548088209a2459217dcdfe78fb59ff7221

--- device.mk
+++ device.mk
@@ -45,6 +45,8 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
+    OPlusFrameworksResCommon \
+    OPlusSettingsResCommon \
     WifiResTarget
 
 # AAPT
