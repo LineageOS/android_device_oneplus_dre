3250dfc3de41da920afc5aa8d81ad477cc0b8a72
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 20:41:13 -0700

    dre: Import CarrierConfig RRO from DE2117_11.C.15
     * Remove anything that the value is already the default,
       or deprecated.
    
    Change-Id: I597a93b580754a7fde51d051accfffca54c22482

--- device.mk
+++ device.mk
@@ -40,6 +40,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_PACKAGES += \
+    CarrierConfigResCommon
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
