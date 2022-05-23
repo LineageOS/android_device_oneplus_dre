1ec22b2d218d8d2d977dfe9766b2a753e1d9a27d
Albert Tang <tangalbert919@yahoo.com>
2022-05-23 14:37:17 -0500

    dre: Inherit dalvik VM config for phones with 4 GB of RAM
    Change-Id: I59927c94dbbf08ac596136f68ed26f9d0d967aa8

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/dre/dre-vendor.mk)
