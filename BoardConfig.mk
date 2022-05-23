14c4279118748b4184550e0af372cb632ce7f34f
Albert Tang <tangalbert919@yahoo.com>
2022-05-23 17:01:15 -0500

    dre: Remove KeyHandler
    The N200 does not have an alert slider, making KeyHandler useless to us.
    
    Change-Id: I9589b730172b3fc79070d75b8183fe9f92447a6e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -218,7 +218,6 @@ VENDOR_SECURITY_PATCH := 2022-03-01
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
--- device.mk
+++ device.mk
@@ -499,11 +499,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.oneplus
 
-# tri-state-key
-PRODUCT_PACKAGES += \
-    KeyHandler \
-    tri-state-key-calibrate
-
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
