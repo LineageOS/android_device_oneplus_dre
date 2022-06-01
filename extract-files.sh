62bec0d0f9ac54a47be7fda1c08ef0671ddc2bc7
LuK1337 <priv.luk@gmail.com>
2022-06-01 19:31:13 +0200

    dre: Unlock aux cameras
    This basically NOPs update_camera_metadata_entry() call that sets
    ANDROID_REQUEST_AVAILABLE_CAPABILITIES_SYSTEM_CAMERA to 1 for aux cams.
    
    Change-Id: Ie869b3eb0ea85af99904e2e23e86c4cd0acc3596

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,10 @@ function blob_fixup() {
         system_ext/lib64/libwfdnative.so)
             sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
             ;;
+        vendor/lib64/hw/com.qti.chi.override.so)
+            "${SIGSCAN}" -p "0E 47 04 94" -P "1F 20 03 D5" -f "${2}"
+            "${SIGSCAN}" -p "84 3A 04 94" -P "1F 20 03 D5" -f "${2}"
+            ;;
     esac
 }
 
