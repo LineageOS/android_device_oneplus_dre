68aad3d64852e77e1dc78c93245dc436b2f05d2f
Albert Tang <tangalbert919@yahoo.com>
2022-06-02 13:58:13 -0500

    dre: Ship FM2
    This device is able to use FM Radio, so we might as well use it.
    
    Change-Id: I791e4ca732feabd543dadc885f4ab0940e1cf3f4

--- device.mk
+++ device.mk
@@ -196,6 +196,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# FM Radio
+PRODUCT_PACKAGES += \
+    FM2 \
+    qcom.fmradio
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/gps.conf:$(TARGET_COPY_OUT_ODM)/etc/gps.conf
