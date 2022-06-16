5d02516f7a90536ab6b883371a00d893ec856048
Albert Tang <tangalbert919@yahoo.com>
2022-06-16 17:37:23 -0500

    dre: Switch to oplus fingerprint HAL
    The generic fingerprint HAL no longer works here.
    
    Change-Id: Ibe16dd5f1e5c30de179c034e15b68ffb4a6f72ec

--- device.mk
+++ device.mk
@@ -214,8 +214,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service \
-    vendor.oneplus.fingerprint.extension@1.0.vendor
+    android.hardware.biometrics.fingerprint@2.3-service.oplus
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
