8d72ba5ea1b7756dc1708c67bfb2c75c9259925f
Albert Tang <tangalbert919@yahoo.com>
2022-07-04 16:07:54 -0500

    dre: Revert "sm8350-common: Autopatch vendor.qti.hardware.camera.postproc impl"
    Not needed, as the method call to be patched out is not present.
    
    This reverts commit c5ef1cac3b9d3f5b8aa0615970e31b596110fd50.
    
    Change-Id: I28a054b13a2f192e020af14b7885977f786a2fb6

--- extract-files.sh
+++ extract-files.sh
@@ -61,9 +61,6 @@ function blob_fixup() {
         vendor/lib64/hw/fingerprint.qcom.so)
             sed -i "s/goodix\.3626/fingerprint/g" "${2}"
             ;;
-        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
-            "${SIGSCAN}" -p "3F 0A 00 94" -P "1F 20 03 D5" -f "${2}"
-            ;;
     esac
 }
 
