58bfd9ea917e93b33ec5fb92ee286964b5a462b5
Edwin Moquete <edwinmmoquete@gmail.com>
2022-08-29 01:37:22 +0000

    dre: Switch to legacy sepolicy_vndr
    Change-Id: I13229c3057bf32c81b0fdace075f0791cb8ad380

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,7 +184,7 @@ BOOT_SECURITY_PATCH := 2022-05-05
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SEPolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
