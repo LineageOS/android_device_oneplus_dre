2ecf2e497a18447cff7b7f25bd5ca46fb8c89466
Albert Tang <tangalbert919@yahoo.com>
2023-04-20 20:15:53 -0500

    dre: Build FMRadio
    Change-Id: Id4b5c9c8496acc9a04cca2c37f751997adb6ca76

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,6 +84,9 @@ TARGET_USES_QTI_MAPPER_EXTENSIONS_1_1 := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
+# FM
+BOARD_HAVE_QCOM_FM := true
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/device_framework_matrix.xml \
