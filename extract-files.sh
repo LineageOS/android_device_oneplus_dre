786305010939fcb56e70c6f24fe42d27a3f30398
LuK1337 <priv.luk@gmail.com>
2023-10-21 10:07:10 +0200

    dre: Update libstdc++.vendor target name
    Change-Id: I9fb48d50df7b8b74d19bfb05b3d9c2ca169bf3e2

--- extract-files.sh
+++ extract-files.sh
@@ -64,6 +64,9 @@ function blob_fixup() {
         vendor/lib64/hw/com.qti.chi.override.so)
             grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
             ;;
+        odm/lib64/libCOppLceTonemapAPI.so|odm/lib64/libaps_frame_registration.so)
+            "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
     esac
 }
 
