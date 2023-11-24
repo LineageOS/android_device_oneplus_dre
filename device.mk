f0ac9ffd8a92f4254a1e1e0b01767659c3e20408
Vivekachooz <vivekachooz@gmail.com>
2023-11-24 19:08:51 +0530

    dre: Import and adapt powerhint from redfin
    * From redfin-user-13-TQ3A.230901.001-10750268-release-keys
    
    Change-Id: I923c1adf70d035c9def0032ccf29530ce92ebe66

--- device.mk
+++ device.mk
@@ -276,6 +276,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal \
