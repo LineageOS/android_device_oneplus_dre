29bd1b8cd24093f56ca67f42b5d33a1de972fee0
Georg Veichtlbauer <georg@vware.at>
2023-12-09 16:40:36 +0100

    dre: Inherit generic overlays from OPlus
    Change-Id: I7ecb6c9ea6636450076918bef6dce9986342ae3e

--- device.mk
+++ device.mk
@@ -27,6 +27,7 @@ BOARD_SHIPPING_API_LEVEL := 30
 BOARD_API_LEVEL := 30
 
 # Overlays
+$(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
