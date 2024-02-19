57bdc981e2cf02be73b4745fa3c9ff53b72dc43a
Georg Veichtlbauer <georg@vware.at>
2024-02-19 08:26:45 +0100

    dre: Implement ALS capture as AIDL service
    Change-Id: I5d7522d2c8783e487ee9fdb7c90424ec2b5c8cab

--- device.mk
+++ device.mk
@@ -271,7 +271,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.oplus-multihal
+    android.hardware.sensors-service.oplus-multihal \
+    vendor.lineage.oplus_als.service
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
