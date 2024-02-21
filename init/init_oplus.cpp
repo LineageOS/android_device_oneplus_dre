7de03cce197cf20d9838f95baeaba97671d6779a
Albert Tang <tangalbert919@yahoo.com>
2024-02-21 17:41:56 -0600

    dre: Update from OOS 11.C.29
    Change-Id: I3d4dfe3257bc5d39a0b4f381571640ff856a02d3

--- init/init_oplus.cpp
+++ init/init_oplus.cpp
@@ -11,8 +11,8 @@
 
 using android::base::GetProperty;
 
-constexpr const char* BUILD_DESCRIPTION = "OnePlusN200TMO-user 12 SKQ1.210216.001 14c4b0e_2-822fa release-keys";
-constexpr const char* BUILD_FINGERPRINT = "OnePlus/OnePlusN200TMO/OnePlusN200TMO:12/SKQ1.210216.001/R.202311232048:user/release-keys";
+constexpr const char* BUILD_DESCRIPTION = "OnePlusN200TMO-user 12 SKQ1.210216.001 160139d-4b43-4b45 release-keys";
+constexpr const char* BUILD_FINGERPRINT = "OnePlus/OnePlusN200TMO/OnePlusN200TMO:12/SKQ1.210216.001/R.202401231829:user/release-keys";
 
 constexpr const char* RO_PROP_SOURCES[] = {
     nullptr,
