594e8e1c208791091412b16630bfc5fc2bcf8a1a
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 02:04:54 +0300

    dre: Work around using prebuilt HWC
    After enabling ELF checks and switching prebuilts to modules,
    the init and vintf files are no longer automatically built.
    
    Change-Id: I48fcab53a7c8128b18e1399d9afadcfd5c85b116

--- device.mk
+++ device.mk
@@ -164,6 +164,8 @@ PRODUCT_PACKAGES += \
     init.qti.display_boot.sh \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
+    vendor.qti.hardware.display.composer-service.rc \
+    vendor.qti.hardware.display.composer-service.xml \
     vendor.qti.hardware.memtrack-service
 
 PRODUCT_COPY_FILES += \
