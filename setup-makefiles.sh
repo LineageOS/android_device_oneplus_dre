ce340bbc57c37e37c7758d28d0c5afa2fa0bae86
Michael Bestas <mkbestas@lineageos.org>
2024-09-29 11:24:29 +0300

    dre: Sync extract scripts with 22 templates
    Change-Id: I7963ae050cfb193c5d0c2636da9c1e8f82bcdbdb

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -29,7 +29,7 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 # Warning headers and guards
 write_headers
 
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
+write_makefiles "${MY_DIR}/proprietary-files.txt"
 
 # Finish
 write_footers
