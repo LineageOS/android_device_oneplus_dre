97fb165d39bcba5903ecf2cfe427c39ec8238c11
LuK1337 <priv.luk@gmail.com>
2024-12-05 16:41:51 +0100

    Revert "dre: Build 64-bit only libvndfwk_detect_jni.qti.vendor"
    Before:
    audio_hw_extn: spkr_prot_feature_init: Called with feature Enabled, vendor_enhanced_info 0x0
    
    After:
    audio_hw_extn: spkr_prot_feature_init: Called with feature Enabled, vendor_enhanced_info 0x3
    
    This reverts commit ebce0090de62586610b30dd477c104af8c06530d.
    
    Change-Id: Ie5c33eafc1e0be2eeda8e0a6bbbc3712e32986e9

--- device.mk
+++ device.mk
@@ -284,7 +284,7 @@ PRODUCT_PACKAGES += \
 
 # QMI
 PRODUCT_PACKAGES += \
-    libvndfwk_detect_jni.qti.vendor:64
+    libvndfwk_detect_jni.qti.vendor # Needed by CNE app
 
 # Sensors
 PRODUCT_PACKAGES += \
