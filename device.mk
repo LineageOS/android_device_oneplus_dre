c2a9564491cda3956f6b22b73564b95599fc1524
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    dre: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- device.mk
+++ device.mk
@@ -249,6 +249,11 @@ PRODUCT_PACKAGES += \
     WifiResTarget
 
 # Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Perf
