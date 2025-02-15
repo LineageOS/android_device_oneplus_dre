98db7f9d696072b27289c4c68201a2796b630ce5
Albert Tang <tangalbert919@yahoo.com>
2025-02-14 16:45:36 -0800

    dre: configs: Drop sRGB display mode
    sRGB support is broken for this panel, so leave native as the only
    option.
    
    Co-authored-by: Nick Reuter <nreuter85@gmail.com>
    Change-Id: I86c4e46e85a63b9fc600168f81b073373cd5924b

--- extract-files.py
+++ extract-files.py
@@ -98,6 +98,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|vendor_audio).*\n', ''),
     'vendor/etc/msm_irqbalance.conf': blob_fixup()
         .regex_replace('IGNORED_IRQ=19,21,38$', 'IGNORED_IRQ=19,21,38,209,218'),
+    'vendor/etc/qdcm_calib_data_nt36672c_tm_fhd_plus_video_mode_dsi_panel.xml': blob_fixup()
+        .patch_file('blob-patches/qdcm-remove-srgb.patch'),
     'vendor/lib64/hw/com.qti.chi.override.so': blob_fixup()
         .add_needed('libcamera_metadata_shim.so'),
 }  # fmt: skip
