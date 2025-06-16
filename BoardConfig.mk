1f8f7c4aaf67e8be5570782706821884872a34e7
basamaryan <basam.aryan@gmail.com>
2025-06-16 11:19:48 -0400

    dre: Drop legacy ANT remnants
    QSSI only has platform/external/ant-wireless/hidl since a while
    
    Change-Id: Ibccfdc7e72efd3c9d3ff62bda3e729b99433a567

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,9 +22,6 @@ AB_OTA_PARTITIONS += \
     vendor_boot \
     vendor_dlkm
 
-# ANT+
-BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
