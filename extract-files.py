11a66d433aabb2981026a2c4249e89d56cf5daaf
basamaryan <basam.aryan@gmail.com>
2025-07-09 08:52:21 -0400

    dre: Update WFD system blobs from dada OS2.0.217.0.WOCMIXM
    Change-Id: I95dfb07cae97f3c8716a7cb6d87a7081de986748

--- extract-files.py
+++ extract-files.py
@@ -72,16 +72,6 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libcrypto_shim.so'),
     'product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml': blob_fixup()
         .regex_replace('/my_product', '/product'),
-    'system_ext/bin/wfdservice': blob_fixup()
-        .add_needed('libwfdservice_shim.so'),
-    'system_ext/lib/libwfdmmsrc_system.so': blob_fixup()
-        .add_needed('libgui_shim.so'),
-    'system_ext/lib/libwfdservice.so': blob_fixup()
-        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
-    'system_ext/lib64/libwfdnative.so': blob_fixup()
-        .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so')
-        .add_needed('libbinder_shim.so')
-        .add_needed('libinput_shim.so'),
     'vendor/bin/init.kernel.post_boot-blair.sh': blob_fixup()
         .patch_file('blob-patches/init-post-boot-blair.patch'),
     'vendor/bin/init.kernel.post_boot-holi.sh': blob_fixup()
