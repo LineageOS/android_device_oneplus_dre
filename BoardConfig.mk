711d4717d62de5315fcdc8ba4a3fda937d2440be
Michael Bestas <mkbestas@lineageos.org>
2025-07-19 10:55:59 +0300

    dre: Migrate QTI power HAL to soong_config_set
    Change-Id: Ib80c15ee523130011147d378f665592dae501ec2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,9 +130,6 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := holi
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
-
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
