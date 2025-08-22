537c0d8fc89da5e7d846affbfb1463773cd36138
LuK1337 <priv.luk@gmail.com>
2025-08-22 09:31:46 +0200

    dre: sepolicy: Migrate /proc/qcom_flash rules to hardware/oplus
    Change-Id: Iba59cbf2ada217cac07f70d6dc4665a928f0591e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,8 +166,6 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
