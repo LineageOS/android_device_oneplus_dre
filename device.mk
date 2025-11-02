942a619f04894f6d851236ec80551e2d5853543a
chandu078 <chandudyavanapelli03@gmail.com>
2025-11-02 15:59:54 +0000

    dre: Move manifests to vintf directory
    Change-Id: I2a82e3ae75a1293318efeea0519cfbe64eb4b364

--- device.mk
+++ device.mk
@@ -386,7 +386,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/oplus/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/vintf/manifest.xml
 
 # WiFi
 PRODUCT_PACKAGES += \
