343f3210149ed562ec4b946b8ecd82e13b8a4201
Nick Reuter <nreuter85@gmail.com>
2025-11-15 16:24:57 -0600

    dre: Update from OOS 11.C.37
    Change-Id: Ib6d64c998e3a5b5a66b87474fc3c629b78e13588

--- lineage_dre.mk
+++ lineage_dre.mk
@@ -34,8 +34,8 @@ PRODUCT_MODEL := DE2117
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="OnePlusN200-user 12 SKQ1.210216.001 R.1a8c53e-1-16457e release-keys" \
-    BuildFingerprint=OnePlus/OnePlusN200/OnePlusN200:12/SKQ1.210216.001/R.1a8c53e-1-16457e:user/release-keys \
+    BuildDesc="OnePlusN200-user 12 SKQ1.210216.001 R.205d809_1-735849 release-keys" \
+    BuildFingerprint=OnePlus/OnePlusN200/OnePlusN200:12/SKQ1.210216.001/R.205d809_1-735849:user/release-keys \
     DeviceName=OnePlusN200 \
     DeviceProduct=OnePlusN200 \
     SystemDevice=OnePlusN200 \
